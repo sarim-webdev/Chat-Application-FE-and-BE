3e50411dcca89693af450fd5e421cd3c91660d22
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-08-08 17:26:23 +0500

    Professional more features in this Chat-Application

--- server/src/app.js
+++ server/src/app.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chat-application-fe-and-be.vercel.app/",
+      "https://chat-application-fe-and-be.vercel.app",
     ],
     credentials: true,
   }),
