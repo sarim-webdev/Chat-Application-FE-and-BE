e129a4e3b465dbb3c28976ac210cb2ba78b033ad
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-09-02 09:37:12 +0500

    Professional more features in this Chat-Application

--- server/api/index.js
+++ server/api/index.js
@@ -1,19 +1,36 @@
 import dotenv from "dotenv";
+import http from "http";
 
 import { connectDb } from "../src/db/db.js";
 import { app } from "../src/app.js";
+import { initSocket } from "../src/socket/socket.js";
 
 dotenv.config();
 
 let dbConnected = false;
+let ioInitialized = false;
+let server;
 
 export default async function handler(req, res) {
   try {
+    // Connect MongoDB
     if (!dbConnected) {
       await connectDb();
       dbConnected = true;
     }
 
+    // Create HTTP server
+    if (!server) {
+      server = http.createServer(app);
+    }
+
+    // Initialize Socket.IO
+    if (!ioInitialized) {
+      initSocket(server);
+      ioInitialized = true;
+    }
+
+    // Handle Express API request
     return app(req, res);
   } catch (error) {
     console.error("Serverless Function Error:", error);
