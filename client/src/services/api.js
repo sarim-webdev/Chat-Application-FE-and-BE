29f59461da173ee9343f3a91b98cc3dd4b6452fe
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-09-02 09:50:18 +0500

    Professional more features in this Chat-Application

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const isProduction = import.meta.env.PROD;
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: isProduction
+    ? import.meta.env.VITE_PROD_API_URL
+    : import.meta.env.VITE_LOCAL_API_URL,
   withCredentials: true,
 });
 
--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
+const socketURL = import.meta.env.PROD
+  ? import.meta.env.VITE_PROD_SOCKET_URL
+  : import.meta.env.VITE_LOCAL_SOCKET_URL;
+
+const socket = io(socketURL, {
   autoConnect: false,
   withCredentials: true,
 });
